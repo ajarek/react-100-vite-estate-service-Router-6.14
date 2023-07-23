4bc5483de2dc7e62989bc475836403eb33f9e026
Ajarek <ajarek@poczta.onet.pl>
2023-07-24 01:06:44 +0200

    added component Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,20 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="info">
+        <h2>Znajdź Swój <span>Wymarzony Dom</span></h2>
+        <p>Witamy w naszym biurze nieruchomości, dla którego znalezienie wymarzonego domu jest dla nas priorytetem. Nasz zespół doświadczonych agentów zapewnia wyjątkową obsługę i pomaga przejść przez złożony proces kupna lub sprzedaży nieruchomości</p>
+        <form className="info-wrapper">
+          <input type="email" name="" id="" placeholder='TwójEmail' />
+          <input type="submit" value="Wyślij" />
+        </form>
+      </div>
+      <div className="baner">
+      
+      </div>
+
+    </div>
   )
 }
 
