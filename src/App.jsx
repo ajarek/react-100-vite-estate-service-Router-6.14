f9ab50861e1242ac33ae94fbc5ae8e04e4d5e979
Ajarek <ajarek@poczta.onet.pl>
2023-07-23 22:15:32 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createContext, useState } from 'react'
+export const AppContext = createContext()
+
+import Main from './layouts/Main/Main'
+import Error from './pages/Error/Error'
+import Home from './pages/Home/Home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        errorElement: <Error />,
+      },
+     
+    ],
+  },
+])
+function App() {
+  
+  return (
+    <div className='App'>
+      <AppContext.Provider
+        value={{}}
+      >
+        <RouterProvider router={router} />
+      </AppContext.Provider>
+    </div>
+  )
+}
+
+export default App
