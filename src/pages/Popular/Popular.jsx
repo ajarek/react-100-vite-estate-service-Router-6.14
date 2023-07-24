ee10edc8e01c2b24638d173baec8149b4b37252b
Ajarek <ajarek@poczta.onet.pl>
2023-07-25 00:43:49 +0200

    added data.json

--- src/pages/Popular/Popular.jsx
+++ src/pages/Popular/Popular.jsx
@@ -1,8 +1,28 @@
 import './Popular.css'
-
+import data from '../../assets/data.json'
 const Popular = () => {
-  return (
-    <div className='popular'>Popular</div>
+  return ( 
+    <div className='popular'>
+      <h1>Popularne <span>Oferty</span> </h1>
+      <div className="popular-wrapper">
+       {data.map(dt=>{
+        return(
+          <div key={dt.id} className="card">
+            <div className="img">
+
+            <img src={dt.src} alt={dt.name} />
+            </div>
+            <p>{dt.name}</p>
+            <p>{dt.adres}</p>
+            <p>{dt.rooms} pok. {dt.area} m <sup>2</sup></p>
+            <p className='price'>{(+dt.price).toFixed(2)} PLN</p>
+            <button>Zobacz Detale</button>
+          </div>
+        )
+       })}
+      </div>
+      
+    </div>
   )
 }
 
